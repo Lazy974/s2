5b4b3c933546a0e6daa473eece1c97daeac8b1eb
HD <hd@Corinnes-MacBook-Pro.local>
2023-09-11 11:23:47 +0200

    1.1

---
+++ index.php
@@ -0,0 +1,29 @@
+<!-- 
+Dans cet exercice, vous apprendrez à manipuler les tableaux php.
+Veillez à bien lire les instructions présentes en commentaires.
+
+Good luck & happy coding !
+-->
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Made with ❤️ by HelloDev</title>
+    <link rel="stylesheet" href="./assets/css/styles.css">
+    <script src="./assets/js/main.js"></script>
+</head>
+
+<body>
+    <h1 class="text-center py-10">Learn PHP - Step 1</h1>
+
+    <!-- Step 1.1 -->
+    <!-- Déclarer une variable MY PRODUCT de type tableau en SNAKE CASE
+    -->
+
+</body>
+
+</html>
